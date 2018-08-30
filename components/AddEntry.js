1f4a2b8ed86895c869427b75e0dfa844c73dba54
Dani <dani.bonilha@live.com>
2018-08-29 23:16:00 -0300

    feat: Styling AddEntry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native';
 import {
-	getMetricMetaInfo, 
+	getMetricMetaInfo,
 	timeToString,
 	getDailyReminderValue
 } from '../utils/helpers';
@@ -13,6 +13,15 @@ import { TextButton } from './TextButton';
 import { submitEntry, removeEntry } from '../utils/api';
 import { connect } from 'react-redux';
 import { addEntry } from '../actions';
+import { purple, white } from '../utils/colors';
+
+const SubmitBtn = ({ onPress }) => (
+	<TouchableOpacity
+		style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
+		onPress={onPress}>
+		<Text style={styles.submitBtnText}>SUBMIT</Text>
+	</TouchableOpacity>
+);
 
 class AddEntry extends Component {
 	state = {
@@ -89,13 +98,13 @@ class AddEntry extends Component {
 
 		if (this.props.alreadyLogged) {
 			return (
-				<View>
+				<View style={styles.center}>
 					<Ionicons
-						name={'ios-happy-outline'}
+						name={Platform.OS === 'ios' ? 'ios-happy-outline' : 'md-happy'}
 						size={100}
 					/>
 					<Text>You already logged your information for today.</Text>
-					<TextButton onPress={this.reset}>
+					<TextButton style={{ padding: 10 }} onPress={this.reset}>
 						Reset
 					</TextButton>
 				</View>
@@ -103,14 +112,14 @@ class AddEntry extends Component {
 		}
 
 		return (
-			<View>
+			<View style={styles.container}>
 				<DateHeader date={new Date().toLocaleDateString()} />
 				{Object.keys(metaInfo).map((key) => {
 					const { getIcon, type, ...rest } = metaInfo[key];
 					const value = this.state[key];
 
 					return (
-						<View key={key}>
+						<View key={key} style={styles.row}>
 							{getIcon()}
 							{type === 'slider'
 								? <UdaciSlider
@@ -129,14 +138,56 @@ class AddEntry extends Component {
 					);
 
 				})}
-				<TextButton onPress={this.submit}>
-					Submit
-				</TextButton>
+				<SubmitBtn onPress={this.submit} />
 			</View>
 		);
 	}
 
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 20,
+		backgroundColor: white
+	},
+	row: {
+		flexDirection: 'row',
+		flex: 1,
+		alignItems: 'center',
+	},
+	iosSubmitBtn: {
+		backgroundColor: purple,
+		padding: 10,
+		borderRadius: 7,
+		height: 45,
+		marginLeft: 40,
+		marginRight: 40,
+	},
+	AndroidSubmitBtn: {
+		backgroundColor: purple,
+		padding: 10,
+		paddingLeft: 30,
+		paddingRight: 30,
+		height: 45,
+		borderRadius: 2,
+		alignSelf: 'flex-end',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	submitBtnText: {
+		color: white,
+		fontSize: 22,
+		textAlign: 'center',
+	},
+	center: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 30,
+		marginRight: 30,
+	},
+});
 
 const mapStateToProps = state => {
 	const key = timeToString();
