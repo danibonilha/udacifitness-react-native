5e0f30a75a52394e2eab4595c6c770d466662694
Dani <dani.bonilha@live.com>
2018-09-02 23:28:27 -0300

    feat: add reactNavigation and create tabs navigator

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
-import History from './components/History';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './store/reducers';
-import AddEntry from './components/AddEntry';
+import { Tabs } from './navigation/Tabs';
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<Provider store={createStore(reducer)}>
 				<View style={{ flex: 1}}>
-					<History />
+					<Tabs />
 				</View>
 			</Provider>
 		);
